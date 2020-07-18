2603e33b9e2231c6ab7617a4563d6adbb0f49956
xsharonhe <xsharonhe@gmail.com>
2020-07-18 19:26:01 -0400

    Adding redirection and email automation

--- app/views.py
+++ app/views.py
@@ -13,23 +13,36 @@ client = None
 MSG_LIMIT = 20
 
 
-@view.route("/login", methods=["POST","GET"])
+@view.route("/")
+@view.route("/login", methods=['GET', 'POST'])
 def login():
     """
     displays main login page and handles saving name in session
     :exception POST
     :return: None
     """
+
     if request.method == "POST":  # if user input a name
         name = request.form["inputName"]
-        if len(name) >= 2:
+        email = request.form["email"]
+        schoolOptions = request.form["schoolOptions"]
+        db = DataBase()
+        db.save_email(name, email, )
+        if len(name) >= 1:
             session[NAME_KEY] = name
-            flash(f'You were successfully logged in as {name}.')
-            return redirect(url_for("views.chat"))
+            flash('You were successfully logged in as {}.'.format(name))
+            if schoolOptions == '1':
+                return redirect(url_for("views.highschool_chat"))
+            if schoolOptions == '2':
+                return redirect(url_for("views.undergraduate_chat"))
+            if schoolOptions == '3':
+                return redirect(url_for("views.graduate_chat"))
+            else:
+                return redirect(url_for("views.chat"))
         else:
             flash("1Name must be longer than 1 character.")
 
-    return render_template("login.html", **{"session":"session"})
+    return render_template("login.html", **{"session": "session"})
 
 
 @view.route("/logout")
@@ -41,36 +54,6 @@ def logout():
     session.pop(NAME_KEY, None)
     flash("You were logged out.")
     return redirect(url_for("views.login"))
-
-
-@view.route("/")
-@view.route("/home")
-def home():
-    """
-    displays home page if logged in
-    :return: None
-    """
-    global client
-
-    if NAME_KEY not in session:
-        return redirect(url_for("views.login"))
-
-    return render_template("index.html", **{"session": session})
-
-
-@view.route("/get_name")
-def get_name():
-    data = {"name": ""}
-    if NAME_KEY in session:
-        data = {"name": session[NAME_KEY]}
-    return jsonify(data)
-
-
-@view.route("/get_messages")
-def get_messages():
-    db = DataBase()
-    msgs = db.get_all_messages(MSG_LIMIT)
-    return jsonify(msgs)
 
 
 @view.route("/chat")
@@ -133,6 +116,22 @@ def graduate_chat():
     """
     return render_template("graduate.html", **{"session": "session"})
 
+# API ENDPOINTS
+@view.route("/get_name")
+def get_name():
+    data = {"name": ""}
+    if NAME_KEY in session:
+        data = {"name": session[NAME_KEY]}
+    return jsonify(data)
+
+
+@view.route("/get_messages")
+def get_messages():
+    db = DataBase()
+    msgs = db.get_all_messages(MSG_LIMIT)
+    return jsonify(msgs)
+
+
 @view.route("/get_history")
 def get_history(name):
     """
@@ -148,10 +147,6 @@ def get_history(name):
 
 @view.route('/clean')
 def robo():
-    """
-    :param name: str
-    :return: all messages by name of user
-    """
     db = DataBase()
     msgs = db.robo_messages(MSG_LIMIT)
     messages = remove_seconds_from_messages(msgs)
