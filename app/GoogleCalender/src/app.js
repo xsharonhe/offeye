d2187b9ebb541c79887eb9d7235157bd152c15f7
Emma-He <emma.xiaotong@gmail.com>
2020-07-18 20:15:44 -0300

    Calendar initial commit

---
+++ app/GoogleCalender/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', __dirname + '/public/views')
+app.set('view engine', 'html')
+app.engine('html', require('ejs').renderFile)
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.post('/', (req, res) => {
+    res.send('Hello from post req.')
+})
+
+app.listen(3000, () => {
+    console.log('Server on port 3000')
+})
